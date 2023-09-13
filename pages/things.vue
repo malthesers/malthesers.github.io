91a77f8212f5f681d432df9042152ba1ce496bd1
malt9830 <malt9830@stud.kea.dk>
2023-09-13 05:25:38 +0200

    Added initial projects to things

--- pages/things.vue
+++ pages/things.vue
@@ -1,7 +1,14 @@
 <template>
   <section>
-    <h1>things i've made</h1>
-    <Icon name="uil:github"/>
+    <h1 class="mb-4">things i've made</h1>
+    <div class="grid md:grid-cols-2 gap-4">
+      <article v-for="thing in things" :key="thing.name" class="bg-dark px-4 py-2 rounded-xl">
+        <h2 class="lowercase">{{ thing.name }}</h2>
+        <a :href="thing.repo" rel="nofollow noreferrer noopener">
+          <Icon name="uil:github"/>
+        </a>
+      </article>
+    </div>
   </section>
 </template>
 
@@ -14,4 +21,21 @@ useSeoMeta({
   // ogImage: 'https://example.com/image.png',
   // twitterCard: 'summary_large_image',
 })
+
+const things = ref([
+  {
+    name: 'Soulsborndle',
+    repo: 'https://github.com/malthesers/soulsborndle',
+    site: 'https://malthesers.github.io/soulsborndle/',
+    desc: '',
+    tools: ['Vue', 'TailwindCSS']
+  },
+  {
+    name: 'Pablo\'s Pizza',
+    repo: 'https://github.com/malthesers/pablos-pizza',
+    site: 'https://malthesers.github.io/pablos-pizza/',
+    desc: '',
+    tools: ['JavaScript', 'TailwindCSS']
+  },
+])
 </script>
