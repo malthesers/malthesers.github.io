e6ad56613648f769b2dde6f681d2a407039bc850
malt9830 <malt9830@stud.kea.dk>
2023-09-13 05:06:39 +0200

    Added 'underslide' class for hover underlines

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,7 +2,7 @@
   <header class="bg-dark font-display font-medium px-6 py-3 flex justify-between">
     <NuxtLink to="/" class="text-xl">malthesers</NuxtLink>
     <nav class="text-lg flex place-items-center">
-      <NuxtLink to="/things">things</NuxtLink>
+      <NuxtLink to="/things" class="underslide">things</NuxtLink>
     </nav>
   </header>
 </template>
