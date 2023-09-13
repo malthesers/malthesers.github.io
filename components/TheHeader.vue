018160bc99700ce0029d195917434f5266694b76
malt9830 <malt9830@stud.kea.dk>
2023-09-13 05:35:54 +0200

    Removed underslide class, instead style in header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,7 +2,31 @@
   <header class="bg-dark font-display font-medium px-6 py-3 flex justify-between">
     <NuxtLink to="/" class="text-xl">malthesers</NuxtLink>
     <nav class="text-lg flex place-items-center">
-      <NuxtLink to="/things" class="underslide">things</NuxtLink>
+      <NuxtLink to="/">test</NuxtLink>
+      <NuxtLink to="/things">things</NuxtLink>
     </nav>
   </header>
 </template>
+
+<style scoped>
+a {
+  position: relative;
+  display: inline-block;
+}
+
+a::after {
+  position: absolute;
+  content: "";
+  width: 100%;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  transition: transform 200ms ease-out;
+  transform: scaleX(0);
+}
+
+a:hover::after,
+.router-link-active::after {
+  transform: scaleX(1);
+}
+</style>
