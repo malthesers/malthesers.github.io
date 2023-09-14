f2e6925d6a6b67339e6792f67b9ffb0badc2ebdf
malt9830 <malt9830@stud.kea.dk>
2023-09-14 10:40:04 +0200

    Added initial mobile header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,12 +1,26 @@
 <template>
-  <header class="bg-dark font-display font-medium px-6 py-3 flex justify-between">
-    <NuxtLink to="/" class="text-xl">malthesers</NuxtLink>
-    <nav class="text-lg flex place-items-center">
-      <NuxtLink to="/">test</NuxtLink>
-      <NuxtLink to="/things">things</NuxtLink>
-    </nav>
+  <header class="fixed z-10 w-full font-display font-medium">
+    <div class="flex justify-between bg-dark px-6 py-3">
+      <NuxtLink to="/" class="text-xl">malthesers</NuxtLink>
+      <button class="sm:hidden text-xl" @click="showMenu = !showMenu">{{ showMenu ? '-' : '+' }}</button>
+      <nav class="hidden sm:flex gap-4 place-items-center text-lg">
+        <NuxtLink to="/">test</NuxtLink>
+        <NuxtLink to="/things">things</NuxtLink>
+      </nav>
+    </div>
+    <aside v-if="showMenu">
+      <div class="flex flex-col gap-4 text-lg bg-dark px-6 py-3">
+        <NuxtLink to="/">test</NuxtLink>
+        <NuxtLink to="/things">things</NuxtLink>
+      </div>
+      <img src="~/assets/images/cavern-transitions/cavern-top-front.svg" alt="header bottom cavern" class="h-20 object-cover">
+    </aside>
   </header>
 </template>
+
+<script setup>
+const showMenu = ref(false)
+</script>
 
 <style scoped>
 nav a {
