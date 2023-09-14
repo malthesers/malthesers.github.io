a16de09a20506d3cb588c8c9cdc5dc394f01d116
malt9830 <malt9830@stud.kea.dk>
2023-09-14 12:02:44 +0200

    Reformatted cavern bottom

--- components/cavern/BottomTransition.vue
+++ components/cavern/BottomTransition.vue
@@ -1,60 +1,8 @@
 <template>
-  <div class="cavern-bottom-transition">
-    <div class="cavern-bottom-back"></div>
-    <div class="cavern-bottom-left"></div>
-    <div class="cavern-bottom-right"></div>
-    <div class="cavern-bottom-front"></div>
+  <div class="relative w-full h-[15rem] pointer-events-none">
+    <div class="cavern-bottom-back absolute bottom-0 w-full h-[24rem] bg-cover bg-no-repeat bg-[50%] bg-[url(assets/images/cavern-transitions/cavern-bottom-back.svg)]"></div>
+    <div class="cavern-bottom-left absolute bottom-0 w-[8rem] h-[17rem] bg-cover bg-no-repeat bg-[url(assets/images/cavern-transitions/cavern-bottom-left.svg)] left-0"></div>
+    <div class="cavern-bottom-right absolute bottom-0 w-[8rem] h-[16rem] bg-cover bg-no-repeat bg-[url(assets/images/cavern-transitions/cavern-bottom-right.svg)] right-0"></div>
+    <div class="cavern-bottom-front absolute bottom-0 w-full h-[24rem] bg-cover bg-no-repeat bg-[24%] bg-[url(assets/images/cavern-transitions/cavern-bottom-front.svg)]"></div>
   </div>
 </template>
-
-<style scoped>
-.cavern-bottom-transition {
-  position: relative;
-  width: 100%;
-  height: 15rem;
-  pointer-events: none;
-}
-
-.cavern-bottom-back {
-  position: absolute;
-  width: 100%;
-  height: 24rem;
-  bottom: 0;
-  background-image: url(assets/images/cavern-transitions/cavern-bottom-back.svg);
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: 24%;
-}
-
-.cavern-bottom-front {
-  position: absolute;
-  width: 100%;
-  height: 24rem;
-  bottom: 0;
-  background-image: url(assets/images/cavern-transitions/cavern-bottom-front.svg);
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: 50%;
-}
-
-.cavern-bottom-left {
-  position: absolute;
-  width: 8rem;
-  height: 17rem;
-  bottom: 0;
-  background-image: url(assets/images/cavern-transitions/cavern-bottom-left.svg);
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.cavern-bottom-right {
-  position: absolute;
-  width: 8rem;
-  height: 16rem;
-  bottom: 0;
-  right: 0;
-  background-image: url(assets/images/cavern-transitions/cavern-bottom-right.svg);
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-</style>
