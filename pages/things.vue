401cd34858d75d992f5650d0aec515297352c931
malt9830 <malt9830@stud.kea.dk>
2023-09-14 12:18:27 +0200

    Aligned tools + github link for things

--- pages/things.vue
+++ pages/things.vue
@@ -3,19 +3,21 @@
     <section>
       <h1 class="mb-4">things i've made</h1>
       <div class="grid md:grid-cols-2 gap-4">
-        <article v-for="thing in things" :key="thing.name" class="bg-dark px-4 py-2 rounded-xl">
+        <article v-for="thing in things" :key="thing.name" class="bg-dark flex flex-col px-4 py-2 rounded-xl">
           <h2 class="lowercase">{{ thing.name }}</h2>
           <p class="mb-4">{{ thing.desc }}</p>
-          <div>
-            <Icon name="vscode-icons:file-type-vue"/>
-            <Icon name="vscode-icons:file-type-tailwind"/>
-            <Icon name="vscode-icons:file-type-html"/>
-            <Icon name="vscode-icons:file-type-css"/>
-            <Icon name="vscode-icons:file-type-js-official"/>
+          <div class="mt-auto flex justify-between">
+            <NuxtLink :to="thing.repo" target="_blank">
+              <Icon name="uil:github"/>
+            </NuxtLink>
+            <div>
+              <Icon v-if="thing.tools.includes('vue')" name="vscode-icons:file-type-vue"/>
+              <Icon v-if="thing.tools.includes('tw')" name="vscode-icons:file-type-tailwind"/>
+              <Icon v-if="thing.tools.includes('html')" name="vscode-icons:file-type-html"/>
+              <Icon v-if="thing.tools.includes('css')" name="vscode-icons:file-type-css"/>
+              <Icon v-if="thing.tools.includes('js')" name="vscode-icons:file-type-js-official"/>
+            </div>
           </div>
-          <NuxtLink :to="thing.repo" target="_blank">
-            <Icon name="uil:github"/>
-          </NuxtLink>
         </article>
       </div>
     </section>
@@ -38,7 +40,7 @@ const things = ref([
     repo: 'https://github.com/malthesers/soulsborndle',
     site: 'https://malthesers.github.io/soulsborndle/',
     desc: 'A Wordle-like Souls-like web game - guess the boss by using hints from your past guesses!',
-    tools: ['vuejs', 'tailwind']
+    tools: ['vue', 'tw']
   },
   {
     name: 'Pablo\'s Pizza',
@@ -52,7 +54,7 @@ const things = ref([
     repo: 'https://github.com/malthesers/hogwarts-vue',
     site: 'https://malthesers.github.io/hogwarts-vue/',
     desc: 'Assume the role of a Hogwarts system administrator (but in Vue).',
-    tools: ['vuejs', 'tailwind']
+    tools: ['vue', 'tw']
   },
   {
     name: 'Hogwarts',
