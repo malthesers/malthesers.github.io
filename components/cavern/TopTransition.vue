d422e7c7c5fcf782fed19e4fe7deec4b30cc87cf
malt9830 <malt9830@stud.kea.dk>
2023-09-14 11:57:36 +0200

    Reformatted cavern top

--- components/cavern/TopTransition.vue
+++ components/cavern/TopTransition.vue
@@ -1,56 +1,8 @@
 <template>
-  <div class="cavern-top-transition">
-    <div class="cavern-top-back"></div>
-    <div class="cavern-top-left"></div>
-    <div class="cavern-top-right"></div>
-    <div class="cavern-top-front"></div>
+  <div class="relative w-full h-[7rem] pointer-events-none">
+    <div class="absolute w-full h-[11rem] bg-cover bg-no-repeat bg-[70%] bg-[url(assets/images/cavern-transitions/cavern-top-back.svg)]"></div>
+    <div class="absolute w-[8rem] h-[17rem] bg-cover bg-no-repeat bg-[url(assets/images/cavern-transitions/cavern-top-left.svg)] left-0"></div>
+    <div class="absolute w-[8rem] h-[16rem] bg-cover bg-no-repeat bg-[url(assets/images/cavern-transitions/cavern-top-right.svg)] right-0"></div>
+    <div class="absolute w-full h-[17rem] bg-cover bg-no-repeat bg-[40%] bg-[url(assets/images/cavern-transitions/cavern-top-front.svg)]"></div>
   </div>
 </template>
-
-<style scoped>
-.cavern-top-transition {
-  position: relative;
-  width: 100%;
-  height: 7rem;
-  pointer-events: none;
-}
-
-.cavern-top-back {
-  position: absolute;
-  width: 100%;
-  height: 11rem;
-  background-image: url(assets/images/cavern-transitions/cavern-top-back.svg);
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: 70%;
-}
-
-.cavern-top-front {
-  position: absolute;
-  width: 100%;
-  height: 17rem;
-  background-image: url(assets/images/cavern-transitions/cavern-top-front.svg);
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: 40%;
-}
-
-.cavern-top-left {
-  position: absolute;
-  width: 8rem;
-  height: 17rem;
-  background-image: url(assets/images/cavern-transitions/cavern-top-left.svg);
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.cavern-top-right {
-  position: absolute;
-  width: 8rem;
-  height: 16rem;
-  right: 0;
-  background-image: url(assets/images/cavern-transitions/cavern-top-right.svg);
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-</style>
--- pages/things.vue
+++ pages/things.vue
@@ -1,20 +1,22 @@
 <template>
   <main>
-    <h1 class="mb-4">things i've made</h1>
-    <section class="grid md:grid-cols-2 gap-4">
-      <article v-for="thing in things" :key="thing.name" class="bg-dark px-4 py-2 rounded-xl">
-        <h2 class="mb-4 lowercase">{{ thing.name }}</h2>
-        <div>
-          <Icon name="vscode-icons:file-type-vue"/>
-          <Icon name="vscode-icons:file-type-tailwind"/>
-          <Icon name="vscode-icons:file-type-html"/>
-          <Icon name="vscode-icons:file-type-css"/>
-          <Icon name="vscode-icons:file-type-js-official"/>
-        </div>
-        <NuxtLink :to="thing.repo" target="_blank">
-          <Icon name="uil:github"/>
-        </NuxtLink>
-      </article>
+    <section>
+      <h1 class="mb-4">things i've made</h1>
+      <div class="grid md:grid-cols-2 gap-4">
+        <article v-for="thing in things" :key="thing.name" class="bg-dark px-4 py-2 rounded-xl">
+          <h2 class="mb-4 lowercase">{{ thing.name }}</h2>
+          <div>
+            <Icon name="vscode-icons:file-type-vue"/>
+            <Icon name="vscode-icons:file-type-tailwind"/>
+            <Icon name="vscode-icons:file-type-html"/>
+            <Icon name="vscode-icons:file-type-css"/>
+            <Icon name="vscode-icons:file-type-js-official"/>
+          </div>
+          <NuxtLink :to="thing.repo" target="_blank">
+            <Icon name="uil:github"/>
+          </NuxtLink>
+        </article>
+      </div>
     </section>
   </main>
 </template>
