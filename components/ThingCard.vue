11ec00ece243927aa33c8166df0d8f3f67e52ce4
malt9830 <malt9830@stud.kea.dk>
2023-09-15 07:23:22 +0200

    Added thing card as separate component

---
+++ components/ThingCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="bg-dark flex flex-col px-4 py-2 rounded-xl">
+    <h2 class="lowercase">{{ thing.name }}</h2>
+    <p class="mb-4">{{ thing.desc }}</p>
+    <div class="mt-auto flex justify-between">
+      <div class="flex gap-2">
+        <NuxtLink :to="thing.repo" target="_blank" class="duration-200 hover:opacity-75">
+          <Icon name="uil:github"/>
+        </NuxtLink>
+        <NuxtLink :to="thing.site" target="_blank" class="duration-200 hover:opacity-75">
+          <Icon name="uil:rocket"/>
+        </NuxtLink>
+      </div>
+      <div class="flex gap-2">
+        <Icon v-if="thing.tools.includes('vue')" name="vscode-icons:file-type-vue"/>
+        <Icon v-if="thing.tools.includes('tw')" name="vscode-icons:file-type-tailwind"/>
+        <Icon v-if="thing.tools.includes('html')" name="vscode-icons:file-type-html"/>
+        <Icon v-if="thing.tools.includes('css')" name="vscode-icons:file-type-css"/>
+        <Icon v-if="thing.tools.includes('js')" name="vscode-icons:file-type-js-official"/>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script setup>
+const props = defineProps({
+  thing: Object
+})
+</script>
--- pages/things.vue
+++ pages/things.vue
@@ -3,27 +3,7 @@
     <section>
       <h1 class="mb-4">things i've made</h1>
       <div class="grid md:grid-cols-2 gap-4">
-        <article v-for="thing in things" :key="thing.name" class="bg-dark flex flex-col px-4 py-2 rounded-xl">
-          <h2 class="lowercase">{{ thing.name }}</h2>
-          <p class="mb-4">{{ thing.desc }}</p>
-          <div class="mt-auto flex justify-between">
-            <div class="flex gap-2">
-              <NuxtLink :to="thing.repo" target="_blank" class="duration-200 hover:opacity-75">
-                <Icon name="uil:github"/>
-              </NuxtLink>
-              <NuxtLink :to="thing.site" target="_blank" class="duration-200 hover:opacity-75">
-                <Icon name="uil:rocket"/>
-              </NuxtLink>
-            </div>
-            <div class="flex gap-2">
-              <Icon v-if="thing.tools.includes('vue')" name="vscode-icons:file-type-vue"/>
-              <Icon v-if="thing.tools.includes('tw')" name="vscode-icons:file-type-tailwind"/>
-              <Icon v-if="thing.tools.includes('html')" name="vscode-icons:file-type-html"/>
-              <Icon v-if="thing.tools.includes('css')" name="vscode-icons:file-type-css"/>
-              <Icon v-if="thing.tools.includes('js')" name="vscode-icons:file-type-js-official"/>
-            </div>
-          </div>
-        </article>
+        <ThingCard v-for="thing in things" :key="thing.name" :thing="thing"/>
       </div>
     </section>
   </main>
