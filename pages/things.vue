c720d624133d47076ceb490ead2d84bf130810ba
malt9830 <malt9830@stud.kea.dk>
2023-09-15 10:28:59 +0200

    Added things to separate file

--- pages/things.vue
+++ pages/things.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script setup>
+import things from '~/assets/things.json'
+
 useSeoMeta({
   title: 'malthesers\' things',
   ogTitle: 'malthesers\' things',
@@ -18,35 +20,4 @@ useSeoMeta({
   // ogImage: 'https://example.com/image.png',
   // twitterCard: 'summary_large_image',
 })
-
-const things = ref([
-  {
-    name: 'Soulsborndle',
-    repo: 'https://github.com/malthesers/soulsborndle',
-    site: 'https://malthesers.github.io/soulsborndle/',
-    desc: 'A Wordle-like Souls-like web game - guess the boss by using hints from your past guesses!',
-    tools: ['vue', 'tw']
-  },
-  {
-    name: 'Pablo\'s Pizza',
-    repo: 'https://github.com/malthesers/pablos-pizza',
-    site: 'https://malthesers.github.io/pablos-pizza/',
-    desc: 'The famous pizzeria, Pablo\'s Pizza, is under attack and Pablo needs your help!',
-    tools: ['html', 'css', 'js']
-  },
-  {
-    name: 'Hogwarts | vue',
-    repo: 'https://github.com/malthesers/hogwarts-vue',
-    site: 'https://malthesers.github.io/hogwarts-vue/',
-    desc: 'Assume the role of a Hogwarts system administrator (but in Vue).',
-    tools: ['vue', 'tw']
-  },
-  {
-    name: 'Hogwarts',
-    repo: 'https://github.com/malthesers/hogwarts',
-    site: 'https://malthesers.github.io/hogwarts/',
-    desc: 'Assume the role of a Hogwarts system administrator.',
-    tools: ['html', 'css', 'js']
-  },
-])
 </script>
