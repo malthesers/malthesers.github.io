4111aa0ba55f8f293e68a5a75a5386e1d75a83cf
malt9830 <malt9830@stud.kea.dk>
2023-12-07 15:04:31 +0100

    Added thing interface + json rearrange

---
+++ interfaces/thing.d.ts
@@ -0,0 +1,10 @@
+interface Thing {
+  name: string,
+  repo: string,
+  site: string,
+  desc: string,
+  image: string
+  tools: Icon[],
+}
+
+type Icon = 'vue' | 'react' | 'js' | 'html' | 'css' | 'tw'
