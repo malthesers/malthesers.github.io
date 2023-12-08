62b7e5ae563c096f54414319fd52b043bb763d23
malt9830 <malt9830@stud.kea.dk>
2023-12-08 16:07:10 +0100

    Added Diable 3 screenshot

--- assets/things.ts
+++ assets/things.ts
@@ -4,7 +4,7 @@ export const things:Thing[] = [
     repo: "https://github.com/malthesers/diable-3",
     site: "https://diable-3.vercel.app/",
     desc: "Hellish Wordle-ish guessing game based on Diablo III - guess the item by its properties!",
-    image: "diable-3",
+    image: "diable-3.jpg",
     tools: ["react", "tw"]
   },
   {
@@ -12,7 +12,7 @@ export const things:Thing[] = [
     repo: "https://github.com/malthesers/soulsborndle",
     site: "https://malthesers.github.io/soulsborndle/",
     desc: "A Wordle-like Souls-like web game - guess the boss by using hints from your past guesses!",
-    image: "soulsborndle",
+    image: "soulsborndle.png",
     tools: ["vue", "tw"]
   },
   {
@@ -20,7 +20,7 @@ export const things:Thing[] = [
     repo: "https://github.com/malthesers/pablos-pizza",
     site: "https://malthesers.github.io/pablos-pizza/",
     desc: "The famous pizzeria, Pablo's Pizza, is under attack and Pablo needs your help!",
-    image: "soulsborndle",
+    image: "soulsborndle.png",
     tools: ["html", "css", "js"]
   },
   {
@@ -28,7 +28,7 @@ export const things:Thing[] = [
     repo: "https://github.com/malthesers/hogwarts-vue",
     site: "https://malthesers.github.io/hogwarts-vue/",
     desc: "Assume the role of a Hogwarts system administrator (but in Vue).",
-    image: "hogwarts",
+    image: "hogwarts.png",
     tools: ["vue", "tw"]
   },
   {
@@ -36,7 +36,7 @@ export const things:Thing[] = [
     repo: "https://github.com/malthesers/hogwarts-react",
     site: "https://malthesers.github.io/hogwarts-react/",
     desc: "Assume the role of a Hogwarts system administrator (but in React).",
-    image: "hogwarts",
+    image: "hogwarts.png",
     tools: ["react", "tw"]
   },
   {
@@ -44,7 +44,7 @@ export const things:Thing[] = [
     repo: "https://github.com/malthesers/hogwarts",
     site: "https://malthesers.github.io/hogwarts/",
     desc: "Assume the role of a Hogwarts system administrator.",
-    image: "hogwarts",
+    image: "hogwarts.png",
     tools: ["html", "css", "js"]
   }
 ]
