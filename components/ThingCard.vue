fb40ce8316db20f1db54217d95e6f1738099271a
malt9830 <malt9830@stud.kea.dk>
2023-12-10 16:33:03 +0100

    Added moon to card back

--- components/ThingCard.vue
+++ components/ThingCard.vue
@@ -5,7 +5,7 @@
       <div class="absolute w-full h-full bg-land rounded-xl overflow-hidden">
         <div class="w-full h-full flex flex-col justify-between border-dark border-8">
           <CavernTopTransition />
-          <p class="text-3xl text-center">m</p>
+          <img src="~/assets/images/moon.png" alt="moon" class="w-20 self-center transform rotate-180">
           <CavernTopTransition class="transform rotate-180" />
         </div>
       </div>
