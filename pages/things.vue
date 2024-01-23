3f782650c5b4e13ba35f12750224eaa2e2da56c8
malt9830 <malt9830@stud.kea.dk>
2024-01-23 03:14:23 +0100

    Added styling for tool filters

--- pages/things.vue
+++ pages/things.vue
@@ -3,9 +3,9 @@
     <section class="space-y-4">
       <h1>things i've made</h1>
       <div class="flex flex-wrap gap-4 justify-center sm:justify-start">
-        <label v-for="tool in tools" :key="tool.name" :for="tool.name">
-          <input v-model="activeFilters" type="checkbox" :value="tool.name" :id="tool.name" class="appearance-none" />
-          <Icon :name="'devicon:' + tool.icon" />
+        <label v-for="tool in tools" :key="tool.name" :for="tool.name" class="cursor-pointer">
+          <input v-model="activeFilters" type="checkbox" :value="tool.name" :id="tool.name" class="peer appearance-none" />
+          <Icon :name="'devicon:' + tool.icon" class="opacity-40 peer-checked:opacity-100 duration-200" />
         </label>
       </div>
       <div class="flex flex-wrap gap-4 justify-center">
