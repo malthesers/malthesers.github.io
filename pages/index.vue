bbbe060418c76a5c831e0011474365717877ec0f
malt9830 <malt9830@stud.kea.dk>
2024-01-24 19:29:26 +0100

    Added things link beneath showcase

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,13 @@
   <main>
     <SplashScreen />
     <CavernTopTransition />
-    <section>
+    <section class="flex flex-col items-center gap-8">
       <div class="flex flex-wrap gap-4 justify-center">
         <TransitionGroup name="flip">
           <ThingCard v-for="(thing, index) in showcasedThings" :key="thing.name" :thing="thing" :index="index" />
         </TransitionGroup>
       </div>
+      <NuxtLink to="things" class="text-xl">more things</NuxtLink>
     </section>
   </main>
 </template>
