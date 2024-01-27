118df99f83756cf0a976bc02bf219aead6a8057b
malt9830 <malt9830@stud.kea.dk>
2024-01-27 04:29:14 +0100

    Updated BottomTransition using new * selector

--- components/cavern/BottomTransition.vue
+++ components/cavern/BottomTransition.vue
@@ -1,16 +1,8 @@
 <template>
-  <div class="relative w-full h-[15rem] pointer-events-none">
-    <div
-      class="cavern-bottom-back absolute bottom-0 w-full h-[24rem] bg-cover bg-no-repeat bg-[50%] bg-[url(assets/images/cavern-transitions/cavern-bottom-back.svg)]"
-    ></div>
-    <div
-      class="cavern-bottom-left absolute bottom-0 w-[8rem] h-[17rem] bg-cover bg-no-repeat bg-[url(assets/images/cavern-transitions/cavern-bottom-left.svg)] left-0"
-    ></div>
-    <div
-      class="cavern-bottom-right absolute bottom-0 w-[8rem] h-[16rem] bg-cover bg-no-repeat bg-[url(assets/images/cavern-transitions/cavern-bottom-right.svg)] right-0"
-    ></div>
-    <div
-      class="cavern-bottom-front absolute bottom-0 w-full h-[24rem] bg-cover bg-no-repeat bg-[24%] bg-[url(assets/images/cavern-transitions/cavern-bottom-front.svg)]"
-    ></div>
+  <div class="relative w-full h-[15rem] pointer-events-none *:absolute *:bottom-0 *:bg-cover *:bg-no-repeat">
+    <div class="w-full h-[24rem] bg-[50%] bg-[url(assets/images/cavern-transitions/cavern-bottom-back.svg)]"></div>
+    <div class="w-full h-[24rem] bg-[24%] bg-[url(assets/images/cavern-transitions/cavern-bottom-front.svg)]"></div>
+    <div class="w-[8rem] h-[17rem] left-0 bg-[url(assets/images/cavern-transitions/cavern-bottom-left.svg)]"></div>
+    <div class="w-[8rem] h-[16rem] right-0 bg-[url(assets/images/cavern-transitions/cavern-bottom-right.svg)]"></div>
   </div>
 </template>
