8b707c73f24e9b3acd78a787c0d8f173e0f1e8a3
malt9830 <malt9830@stud.kea.dk>
2024-02-04 20:11:15 +0100

    Converted footer icons to NuxtLinks

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,10 +2,14 @@
   <footer>
     <CavernBottomTransition />
     <div class="bg-dark py-20 px-4">
-      <p class="text-center">
-        <Icon name="mdi:linkedin" />
-        <Icon name="mdi:github-box" />
-      </p>
+      <div class="flex justify-center">
+        <NuxtLink to="https://www.linkedin.com/in/malthe-kusk/" target="_blank">
+          <Icon name="mdi:linkedin" class="duration-200 hover:opacity-75" />
+        </NuxtLink>
+        <NuxtLink to="https://github.com/malthesers" target="_blank">
+          <Icon name="mdi:github-box" class="duration-200 hover:opacity-75" />
+        </NuxtLink>
+      </div>
     </div>
   </footer>
 </template>
