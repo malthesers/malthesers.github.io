5c50db24738be4c4ba06e960d5f1f80e716e80b6
malt9830 <malt9830@stud.kea.dk>
2024-02-04 20:15:16 +0100

    Upscaled footer icons

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,11 +3,11 @@
     <CavernBottomTransition />
     <div class="bg-dark py-20 px-4">
       <div class="flex justify-center">
-        <NuxtLink to="https://www.linkedin.com/in/malthe-kusk/" target="_blank">
-          <Icon name="mdi:linkedin" class="duration-200 hover:opacity-75" />
+        <NuxtLink to="https://www.linkedin.com/in/malthe-kusk/" target="_blank" class="duration-200 hover:opacity-75">
+          <Icon name="mdi:linkedin" size="50" />
         </NuxtLink>
-        <NuxtLink to="https://github.com/malthesers" target="_blank">
-          <Icon name="mdi:github-box" class="duration-200 hover:opacity-75" />
+        <NuxtLink to="https://github.com/malthesers" target="_blank" class="duration-200 hover:opacity-75">
+          <Icon name="mdi:github-box" size="50" />
         </NuxtLink>
       </div>
     </div>
