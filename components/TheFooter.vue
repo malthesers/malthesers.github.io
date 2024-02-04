27d62861ee25698ae2c7289d5ad43e1e8b03f689
malt9830 <malt9830@stud.kea.dk>
2024-02-04 20:03:59 +0100

    Added footer icons

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,7 +2,10 @@
   <footer>
     <CavernBottomTransition />
     <div class="bg-dark py-20 px-4">
-      <p class="text-center">footer</p>
+      <p class="text-center">
+        <Icon name="mdi:linkedin" />
+        <Icon name="mdi:github-box" />
+      </p>
     </div>
   </footer>
 </template>
